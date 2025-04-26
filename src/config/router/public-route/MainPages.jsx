7c5e73e35bcbd965053d72c9ea5a59fae730c511
TinaMahmoudi1 <tinamahmoudi201@gmail.com>
2025-04-26 09:12:17 -0800

    r

--- src/config/router/public-route/MainPages.jsx
+++ src/config/router/public-route/MainPages.jsx
@@ -1,8 +1,6 @@
 import React, { Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
-<<<<<<< HEAD
 import {CourseDetail} from "../../../pages/CourseDetail";
-=======
 import { BlogeList } from "../../../components/BlogeList/BlogeList";
 import { Body } from "../../../components/CourseList/Body";
 
@@ -11,9 +9,6 @@ const Root = React.lazy(() => import("../../../app/layout/Landing/Root"));
 const CourseDetail = React.lazy(() => import("../../../pages/CourseDetail"));
 // const Body = React.lazy(() => import("../../../components/CourseList/Body"));
 // const BlogeList = React(() => import("../../../components/BlogeList/BlogeList"));
-
-
-
 const Spinner = () => (
   <div className="flex justify-center items-center h-screen">
     <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
@@ -23,7 +18,6 @@ const LazyWrapper = ({ children }) => (
   <Suspense fallback={<Spinner />}>{children}</Suspense>
 ); 
 
->>>>>>> a84d81b8f5eff428f8bb6dab8c8b69153ccfe8fc
 
 export const mainPages = {
   path: "/",
@@ -35,10 +29,8 @@ export const mainPages = {
       element: <LazyWrapper><Landing /></LazyWrapper>,
     },
     {
-<<<<<<< HEAD
       path: "/course-detail/:CourseId",
       element: <CourseDetail />,
-=======
       path: "/course-detail",
       element: <LazyWrapper><CourseDetail /></LazyWrapper>,
     },
@@ -49,7 +41,6 @@ export const mainPages = {
     {
       path: "/BlogeList",
       element: <BlogeList />,
->>>>>>> a84d81b8f5eff428f8bb6dab8c8b69153ccfe8fc
     },
   ],
 };
