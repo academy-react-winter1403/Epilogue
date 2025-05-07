710e43ea6fc0b5ab1560e04cc250718b619a5df9
melikabarzin <melikabarzin58@gmail.com>
2025-05-07 11:08:02 +0330

    starRating

--- src/core/hooks/courseHooks/useFavoriteMutation.js
+++ src/core/hooks/courseHooks/useFavoriteMutation.js
@@ -2,28 +2,6 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'react-hot-toast'
 import { postAddCourseFavorite, deleteCourseFavorite } from '../../services/api/courseDetail/postAddCourseFavorite'
 
-// export const useFavoriteMutation = (CourseId, isFav, userFavoriteId) => {
-//     const queryClient = useQueryClient();
-
-//     const query = useMutation({
-//         mutationFn: async () => {
-//           if (isFav) {
-//             return await deleteCourseFavorite(userFavoriteId);
-//           } else {
-//             return await postAddCourseFavorite(CourseId);
-//           }
-//         },
-//         onSuccess: (data) => {
-//           queryClient.invalidateQueries(['courseDetails', CourseId]);
-//           toast.success('با موفقیت انجام شد');
-//         },
-//         onError: (error) => {
-//           toast.error('خطا در انجام عملیات');
-//           console.error('Error:', error);
-//         }
-//       });
-//   return query
-// }
 export const useFavoriteMutation = (CourseId, isFav, userFavoriteId) => {
   const queryClient = useQueryClient();
 
