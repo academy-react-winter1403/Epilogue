2289fc241d3ba8fa47be7e3cba422348bf998ab5
melikabarzin <melikabarzin58@gmail.com>
2025-05-29 17:29:00 +0330

    likeblog

--- src/components/common/LikeDislikeToggle.jsx
+++ src/components/common/LikeDislikeToggle.jsx
@@ -4,52 +4,53 @@ import likeIcone1 from '../../assets/icons/like1.svg'
 import dislikeIconActiv from '../../assets/icons/dislike2.svg'
 
 const LikeDislikeToggle = ({
-    currentUserLike,
-    currentUserDissLike,
-    liked,
-    disliked,
-    delLike,
-    userLikeId
-  }) => {
+  currentUserLike,
+  currentUserDissLike,
+  liked,
+  disliked,
+  delLike,
+  userLikeId
+}) => {
+
+  const hasLiked = currentUserLike === true;
+  const hasDisliked = currentUserDissLike === true || currentUserDissLike === '1' ;
   
+  const handleLike = () => {
+    if (currentUserLike == true) {
+      delLike.mutate(userLikeId);
+    } else {
+      liked.mutate();
+    }
   
-    const hasLiked = currentUserLike === true;
-    const hasDisliked = currentUserDissLike === true;
-    
-    const handleLike = () => {
-      if (currentUserLike == true) {
-        delLike.mutate(userLikeId);
-      } else {
-        liked.mutate();
-      }
-    
-    };
-  
-    return (
-      <div className="flex gap-4">
-        <button
-          onClick={handleLike}
-          className="w-[56px] h-[56px] flex items-center rounded transition-all duration-200"
-        >
-          <img 
-            src={currentUserLike == true ? likeIconeActive : likeIcone1} 
-            alt={hasLiked ? 'Remove like' : 'Like'} 
-            className="cursor-pointer"
-          />
-        </button>
-  
-        <button
-          onClick={() => disliked.mutate()}
-          className="w-[56px] h-[56px] flex items-center rounded cursor-pointer transition-all duration-200"
-        >
-          <img 
-            src={currentUserDissLike === true ? dislikeIconActiv : dislikeIcon} 
-            alt={hasDisliked ? 'Remove dislike' : 'Dislike'} 
-            className="cursor-pointer"
-          />
-        </button>
-      </div>
-    );
+  };
+
+  return (
+    <div className="flex gap-4">
+      <button
+        onClick={handleLike}
+        className="w-[56px] h-[56px] flex items-center rounded transition-all duration-200"
+      >
+        <img 
+          src={currentUserLike == true ? likeIconeActive : likeIcone1} 
+          alt={hasLiked ? 'Remove like' : 'Like'} 
+          className="cursor-pointer"
+        />
+      </button>
+
+
+      <button
+        onClick={() => disliked.mutate() }
+        disabled={hasDisliked}
+        className={`w-[56px] h-[56px] flex items-center rounded transition-all duration-200 ${hasDisliked ? 'opacity-60 cursor-not-allowed' : ''}`}
+      >
+        <img 
+          src={hasDisliked ? dislikeIconActiv : dislikeIcon} 
+          alt={hasDisliked ? 'Remove dislike' : 'Dislike'} 
+          className={hasDisliked ? "cursor-not-allowed" : "cursor-pointer"}
+      />
+      </button>
+    </div>
+  );
 };
 
 export { LikeDislikeToggle }
