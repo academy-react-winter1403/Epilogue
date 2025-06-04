dfc100094f9b0cbf2f478495a25886bed04dfcb2
melikabarzin <melikabarzin58@gmail.com>
2025-06-04 09:55:16 +0330

    .

--- src/components/courseDetail/DetailPage.jsx
+++ src/components/courseDetail/DetailPage.jsx
@@ -10,7 +10,7 @@ const DetailPage = () => {
   
   return (
     <div className='w-auto bg-white'>
-        <div className="max-w-[95rem] mx-auto md:pt-8 pt-4 flex flex-col justify-center">
+        <div className="max-w-[95rem] mx-auto md:pt-8 pt-4 flex flex-col justify-center gap-8">
 
             {/* detail */}
             <DetailCourse CourseId={course.courseId} course={course}/>
