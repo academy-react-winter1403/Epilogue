87213249dc6adae21d49c9fe503f624b0bc01fd6
TinaMahmoudi1 <tinamahmoudi201@gmail.com>
2025-06-15 11:33:15 -0800

    -

--- src/components/common/AnimatedThemeSwitcher.jsx
+++ src/components/common/AnimatedThemeSwitcher.jsx
@@ -3,9 +3,10 @@ import { Moon02Icon } from "./Icons/MoonIcon";
 import { AnimatePresence, motion } from "framer-motion";
 
 const ThemeToggle = () => {
+  
   const getInitialTheme = () => {
     const saved = localStorage.getItem("theme");
-    if (saved) return JSON.parse(saved);
+    // if (saved) return JSON.parse(saved);
 
     const hour = new Date().getHours();
     const isDayTime = hour >= 7 && hour <= 19;
